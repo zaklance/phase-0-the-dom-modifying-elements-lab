706e76ea6441739acffbfc64b247c48d50c7adbb
zaklance <wosewick@gmail.com>
2024-04-13 17:09:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove(main);
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "is the champion"
